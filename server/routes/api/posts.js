8c759b08ff1ec8ba4974a3f9fae5ca1a10502176
damari0 <christ@utilmate.com>
2020-10-21 09:10:54 +1300

    initial commit

---
+++ server/routes/api/posts.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+// GET post
+router.get('/', async (req, res) => {
+	const posts = await loadPostsCollection();
+	res.send(await posts.find({}).toArray());
+});
+
+// ADD post
+router.post('/', async (req, res) => {
+	const posts = await loadPostsCollection();
+	await posts.insertOne({
+		text: req.body.text,
+		createdAt: new Date()
+	});
+	res.status(201).send();
+});
+
+
+// DELETE post
+router.delete('/:id', async (req, res) => {
+	const posts = await loadPostsCollection();
+	await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
+	res.status(200).send();
+})
+
+async function loadPostsCollection() {
+	const client = await mongodb.MongoClient.connect(
+		'mongodb+srv://christian:damars0nBars@seer29.tipk9.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+		useNewUrlParser: true
+	});
+
+	return client.db('seer29').collection('posts');
+}
+
+module.exports = router;
