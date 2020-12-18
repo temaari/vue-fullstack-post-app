be098fcaa456f5fd7068d6df2165faa62c8e6936
temaari <53286793+temaari@users.noreply.github.com>
2020-12-18 21:34:09 +1300

    getting ready to deploy

--- client/src/PostService.js
+++ client/src/PostService.js
@@ -1,34 +1,38 @@
-import axios from 'axios';
+import axios from "axios";
 
-const url = 'api/posts/';
+const url = "http://localhost:3000/api/posts/";
+
+/* eslint-disable no-async-promise-executor */
 
 class PostService {
-	//GET Posts
-	static getPosts() {
-		return new Promise(async (resolve, reject) => {
-			try {
-				const res = await axios.get(url);
-				const data = res.data;
-				resolve(
-					data.map(post => ({
-						...post,
-						createdAt: new Date(post.createdAt)
-					}))
-				);
-			} catch(err) {
-				reject(err);
-			}
-		})
-	}
-	//CREATE Posts
-	static insertPost(text) {
-		return axios.post(url, { text });
-	}
+  // get posts
+  static getPosts() {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const res = await axios.get(url);
+        const data = res.data;
+        resolve(
+          data.map((post) => ({
+            ...post,
+            createdAt: new Date(post.createdAt),
+          }))
+        );
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+  //Create post
+  static insertPost(text) {
+    return axios.post(url, {
+      text,
+    });
+  }
 
-	//DELETE Posts
-	static deletePost(id) {
-		return axios.delete(`${url}${id}`);
-	}
+  //delete post
+  static deletePost(id) {
+    return axios.delete(`${url}${id}`);
+  }
 }
 
 export default PostService;
